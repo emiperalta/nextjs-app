faddecc19e0259dc962d653472dc15cfcc251fce
EmiPeralta <emilianop96@hotmail.com>
2020-12-18 15:34:19 -0300

    undertanding dynamic routes and api routes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 import Layout, { siteTitle } from '../components/layout';
+import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
@@ -14,33 +16,29 @@ export async function getStaticProps() {
     };
 }
 
-export default function Home(props) {
+export default function Home({ allPostsData }) {
     return (
         <Layout home>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>I'm a jr web dev.</p>
-                <p>
-                    (This is a sample website - you’ll be building a site like
-                    this on{' '}
-                    <a href='https://nextjs.org/learn'>our Next.js tutorial</a>
-                    .)
-                </p>
+                <p>[Your Self Introduction]</p>
             </section>
             <section
                 className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
             >
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
-                    {props.allPostsData.map(({ id, date, title }) => (
+                    {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
-                            {title}
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
                             <br />
-                            {id}
-                            <br />
-                            {date}
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
                         </li>
                     ))}
                 </ul>
